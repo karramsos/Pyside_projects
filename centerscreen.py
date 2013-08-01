4c145c8ed98f285ae12604f490c791a2551da7b6
Sukhvinder Singh <karramsos@gmail.com>
2013-08-01 16:31:44 +0200

    First commit

---
+++ centerscreen.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This program centers a window on the screen.
+
+author : Sukhvinder Singh | karramsos@gmail.com | @karramsos
+
+"""
+
+import sys
+from PySide import QtGui
+
+class Example(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Example, self).__init__()
+        
+        self.initUI()
+        
+    def initUI(self):
+        
+        self.resize(250, 150)
+        self.center()
+        
+        self.setWindowTitle('Center')
+        self.show()
+        
+    def center(self):
+        
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+def main():
+    
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+    
+if __name__ == '__main__':
+    main()
+
+    
+    
+    
